d6cf2d310a80aef4d1761805e41dfc497606846c
Mohamedwaheed221 <panther221b@gmail.com>
2024-02-01 03:35:10 -0800

    0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+def square_matrix_simple(matrix=[]):
+    
+    result_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+
+    # Iterate through each element in the matrix and compute the square
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result_matrix[i][j] = matrix[i][j] ** 2
+
